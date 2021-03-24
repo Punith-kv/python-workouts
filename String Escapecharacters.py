a77559bb805df2829763783f190b4db3d21b7f91
Punith-kv <77767538+Punith-kv@users.noreply.github.com>
2021-03-24 23:15:53 +0530

    Add files via upload

---
+++ String Escapecharacters.py
@@ -0,0 +1,22 @@
+#these are the escape sequences of string
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+txt = 'It\'s alright.' #Single Quote
+print(txt)
+txt = "This will insert one \\ (backslash)." #doublequote
+print(txt)
+txt = "Hello\nWorld!" #nextline
+print(txt) 
+txt = "Hello\rWorld!"#carry return
+print(txt) 
+txt = "Hello\tWorld!"#nexttab
+print(txt)
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt) 
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
